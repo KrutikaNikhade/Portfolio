412e3d639114b68c9e554cf1f90fb1fb6cfee85d
krutika nikhade <krutikanikhade0204@gmail.com>
2025-05-29 17:38:44 +0530

    Add Navbar, StarBackground, ThemeToggle components and update styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { NotFound } from "./pages/NotFound";
 
+
+
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Routers>
-          <Router index element={<home />} />
-          <Router path="*" element={<NotFound /> }/>
-        </Routers>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="*" element={<NotFound /> }/>
+        </Routes>
       </BrowserRouter>
     </>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,22 @@
+import{ ThemeToggle } from "../components/ThemeToggle";
+import { StarBackground } from "../components/StarBackground";
+import { Navbar } from "../components/Navbar";
 
 export const Home = () => {
-    return <div className="min-h-screen">  
-        
-        {/* theme toggle*/}
+    return (
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">  
+       {/* theme toggle*/}
+         <ThemeToggle/>
 
         {/* BG effects*/}
+        <StarBackground/>
 
         {/* navbar*/}
+        <Navbar/>
 
         {/* Main content*/}
 
         {/* Footer */}
-        </div>;
+        </div>
+    );
 };
